a4f42eaab9e1e92eed296fa50d27e374dd95e0fd
yamanidev <elamine.yamani@gmail.com>
2023-09-28 13:10:57 +0100

    Pick done

--- playground/easy/00004-easy-pick.ts
+++ playground/easy/00004-easy-pick.ts
@@ -31,7 +31,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [X in K]: T[X]
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
